475de38c743222d43a26a2033d6b9c80833d397f
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-10-18 01:49:50 +0000

    Added navigation bar component and css.

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Home from './pages/home';
+import Navbar from './components/navbar';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return <Home />;
+    return (
+      <div>
+        <Navbar />
+        <Home />
+      </div>
+    );
   }
 }
--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav>
+      <nav className="navbar navbar-dark bg-dark">
         <div className="container">
-          <a>
+          <a className="navbar-brand" href="#">
             Puber
           </a>
           <div>
-            <a href="#sign-in">
+            <a href="#sign-in" className="btn btn-dark">
               Sign In
             </a>
-            <a href="#sign-up">
+            <a href="#sign-up" className="btn btn-dark">
               Sign Up
             </a>
           </div>
