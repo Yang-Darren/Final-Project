6380ca70f153be6919e8f5cfbbca14db64aad44c
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-11-08 05:29:12 +0000

    Added city to database table.

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,3 @@
-insert into "locations" ("locationName", "locationAddress", "lat", "lng")
-values ('Learningfuze', '9200 Irvine Center Dr', '33.634870', '-117.740450')
+insert into "locations" ("locationName", "locationAddress", "city", "lat", "lng")
+values ('Learningfuze', '9200 Irvine Center Dr', 'Irvine', '33.634870', '-117.740450')
 returning *;
