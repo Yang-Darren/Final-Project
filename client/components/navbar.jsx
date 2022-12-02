238ead3283e19060744a200d4ce1b38d4977e1d6
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-12-02 05:06:06 +0000

    Added context to navbar component and renders on if user is logged in.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
 
 export default class Navbar extends React.Component {
   render() {
+    const { user, handleSignOut } = this.context;
     return (
       <nav className="navbar navbar-dark bg-dark">
         <div className="container">
@@ -9,15 +11,26 @@ export default class Navbar extends React.Component {
             Puber
           </a>
           <div>
-            <a href="#sign-in" className="btn btn-dark">
-              Log in
-            </a>
-            <a href="#sign-up" className="btn btn-light">
-              Sign Up
-            </a>
+            {user !== null &&
+              <button className="btn btn-dark" onClick={handleSignOut}>
+                Sign out
+                <i className="ms-2 fas fa-sign-out-alt" />
+              </button>
+            }
+            {user === null &&
+              <>
+                <a href="#sign-in" className="btn btn-dark">
+                  Log in
+                </a>
+                <a href="#sign-up" className="btn btn-light">
+                  Sign Up
+                </a>
+              </>
+            }
           </div>
         </div>
       </nav>
     );
   }
 }
+Navbar.contextType = AppContext;
