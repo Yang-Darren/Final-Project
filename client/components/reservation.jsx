e4bb4a16c7e73dfdf7de1b89243f3ac002587055
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-12-10 05:00:06 +0000

    import react and export default for reservation.

---
+++ client/components/reservation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.autocomplete = null;
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
