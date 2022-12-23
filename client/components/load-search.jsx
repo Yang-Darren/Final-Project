31bad66921cde91ca006adc10055f8e9c2736edf
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-12-23 06:57:59 +0000

    added return to handleReserve if locations are available.

--- client/components/load-search.jsx
+++ client/components/load-search.jsx
@@ -69,6 +69,20 @@ function PlacesAutocomplete() {
   };
 
   const handleReserve = () => {
+    if (availableLocations) {
+      return (
+        <ul id="search-results">
+          <div className="d-flex">
+            <i className={locationType} />
+            <div>
+              <h3 className=''>{availableLocations.locationName}</h3>
+              <p className=''>{availableLocations.locationAddress}</p>
+              <button onClick={handleReserve} className='btn btn-light'>Reserve Now</button>
+            </div>
+          </div>
+        </ul>
+      );
+    }
   };
 
   let locationType = '';
