9a89b8350b392b9e2c3a011e43b7f487b1344bdd
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2023-01-07 07:27:20 +0000

    removed unused http request.

--- server/index.js
+++ server/index.js
@@ -94,39 +94,6 @@ app.post('/api/auth/sign-in', (req, res, next) => {
     .catch(err => next(err));
 });
 
-app.post('/api/reservations', (req, res, next) => {
-  const { username } = req.body;
-  if (!username) {
-    throw new ClientError(401, 'invalid login');
-  }
-  const sql = `
-    select "userId",
-           "hashedPassword"
-      from "users"
-     where "username" = $1
-  `;
-  const params = [username];
-  db.query(sql, params)
-    .then(result => {
-      const [user] = result.rows;
-      if (!user) {
-        throw new ClientError(401, 'invalid login');
-      }
-      const { userId, hashedPassword } = user;
-      return argon2
-        .verify(hashedPassword)
-        .then(isMatching => {
-          if (!isMatching) {
-            throw new ClientError(401, 'invalid login');
-          }
-          const payload = { userId, username };
-          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
-          res.json({ token, user: payload });
-        });
-    })
-    .catch(err => next(err));
-});
-
 app.use(authorizationMiddleware);
 app.use(errorMiddleware);
 
